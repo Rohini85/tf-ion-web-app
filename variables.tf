c297ca7bee99c9355f790b26a80baba3f3e5b8cd
Rohini85 <99239234+Rohini85@users.noreply.github.com>
2022-03-15 22:44:59 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "ami"{
-    default = " ami-008652f2a99e7492c"  #AMI from Packer
+    default = "ami-008652f2a99e7492c"  #AMI from Packer
 }
 variable "instance_type"{
     default="t2.micro"
